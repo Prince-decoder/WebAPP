a082bafc518edb2b13efca955519713b71aa3910
Ashutosh <22princeyadav214@gmail.com>
2026-08-03 18:17:22 +0530

    Creating Security Configuration

---
+++ src/main/java/com/ashu/WebApplication/Security/Security_Config.java
@@ -0,0 +1,10 @@
+package com.ashu.WebApplication.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class Security_Config {
+    
+}
