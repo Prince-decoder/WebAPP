cba5d2cab1c96c721165ce10005b18858cb34a39
Ashutosh <22princeyadav214@gmail.com>
2026-08-04 09:11:39 +0530

    security update to fetch data from database

--- src/main/java/com/ashu/WebApplication/Security/Security_Config.java
+++ src/main/java/com/ashu/WebApplication/Security/Security_Config.java
@@ -1,15 +1,35 @@
 package com.ashu.WebApplication.Security;
 
 import com.ashu.WebApplication.Security.Model.User;
+import com.ashu.WebApplication.Security.Model.UserDetailImplementation;
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class Security_Config {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authProvider()
+    {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return provider;
+    }
 }
