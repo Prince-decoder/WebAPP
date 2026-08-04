ddffbc9c1dede6da7d448940ac54f9f770700796
Ashutosh <22princeyadav214@gmail.com>
2026-08-04 13:04:35 +0530

    adding anotation to autogenerate id

--- src/main/java/com/ashu/WebApplication/Security/Model/User.java
+++ src/main/java/com/ashu/WebApplication/Security/Model/User.java
@@ -1,9 +1,6 @@
 package com.ashu.WebApplication.Security.Model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "Users")
 public class User {
     @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Integer id;
     @Column(name = "name")
     private String username;
