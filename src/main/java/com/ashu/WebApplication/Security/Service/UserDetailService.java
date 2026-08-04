9fd0d7f8bac91727223b87dd97f1bb7f18ec20d6
Ashutosh <22princeyadav214@gmail.com>
2026-08-04 11:49:15 +0530

    Adding service class to interact with repo and store user who is currently using web feature

---
+++ src/main/java/com/ashu/WebApplication/Security/Service/UserDetailService.java
@@ -0,0 +1,31 @@
+package com.ashu.WebApplication.Security.Service;
+
+import com.ashu.WebApplication.Security.Model.User;
+import com.ashu.WebApplication.Security.Model.UserDetailImplementation;
+import com.ashu.WebApplication.Security.Repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        User user= clientRepository.findByUsername(username);
+        if(user==null)
+        {
+            throw new UsernameNotFoundException(username);
+        }
+        else
+        {
+            return new UserDetailImplementation(user);
+        }
+    }
+}
