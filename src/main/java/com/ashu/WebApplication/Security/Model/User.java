58663214ca6618e07275a78e158bd05a102afa11
Ashutosh <22princeyadav214@gmail.com>
2026-08-04 07:07:02 +0530

    Adding default Password for future case

--- src/main/java/com/ashu/WebApplication/Security/Model/User.java
+++ src/main/java/com/ashu/WebApplication/Security/Model/User.java
@@ -3,11 +3,15 @@ package com.ashu.WebApplication.Security.Model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     private Integer id;
--- src/main/java/com/ashu/WebApplication/Security/Repository/ClientRepository.java
+++ src/main/java/com/ashu/WebApplication/Security/Repository/ClientRepository.java
@@ -2,8 +2,10 @@ package com.ashu.WebApplication.Security.Repository;
 
 import com.ashu.WebApplication.Security.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
+@EnableJpaRepositories
+public interface ClientRepository extends JpaRepository<User,Integer> {
 }
